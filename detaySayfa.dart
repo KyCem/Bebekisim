11fff90d17fe37abd6d651842539643f94068397
KyCem <112523026+KyCem@users.noreply.github.com>
2022-12-31 17:34:57 +0300

    Update detaySayfa.dart

--- detaySayfa.dart
+++ detaySayfa.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,10 +16,17 @@ class MyWidget extends StatelessWidget {
       body: ListView.builder(
           itemCount: list.length,
           itemBuilder: (BuildContext ctx, int index) {
-            return Container(
-              height: 40,
-              width: 75,
-              child: Text(list[index]),
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 2),
+              child: Container(
+                height: 40,
+                width: 75,
+                child: Center(
+                    child: Text(
+                  list[index],
+                  style: TextStyle(fontFamily: "VesperLibre", fontSize: 16),
+                )),
+              ),
             );
           }),
     );
